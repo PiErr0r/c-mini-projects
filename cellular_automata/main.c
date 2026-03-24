364c3e9edc803a56f517a0f6796da510a31adb18
Piero Anić <piero.anic@gmail.com>
2026-03-24 09:50:02 +0100

    + rulestring

--- cellular_automata/main.c
+++ cellular_automata/main.c
@@ -5,7 +5,7 @@
 #define H 1200
 #define DT 1e-3
 
-#define CELL_SIZE 15
+#define CELL_SIZE 10
 #define ROW_SIZE (W / CELL_SIZE)
 #define COL_SIZE (H / CELL_SIZE)
 #define GRID_SIZE (ROW_SIZE * COL_SIZE)
@@ -14,7 +14,12 @@ bool grid[GRID_SIZE] = {0};
 
 #define AT(i, j) grid[(j) + (i) * ROW_SIZE]
 #define ATS(Xgrid, i, j) (Xgrid[(j) + (i) * ROW_SIZE])
-#define ATW(y, x) grid[(x/CELL_SIZE) + (y/CELL_SIZE) * ROW_SIZE]
+#define ATW(y, x) grid[(int)(x/CELL_SIZE) + (int)(y/CELL_SIZE) * ROW_SIZE]
+
+#define SET_B(bs, n) (*bs) |= (1 << (n +  0))
+#define SET_S(bs, n) (*bs) |= (1 << (n + 16))
+#define IS_SET_B(bs, n) (*bs) & (1 << (n +  0))
+#define IS_SET_S(bs, n) (*bs) & (1 << (n + 16))
 
 void clear_bg(void) {
     memset(grid, 0, GRID_SIZE * sizeof(bool));
@@ -30,9 +35,12 @@ unsigned int custom_bad_random() {
     return r;
 }
 
-void draw_grid(void) {
+void draw_grid(bool playing) {
     for (size_t i = 0; i < COL_SIZE; ++i) {
         for (size_t j = 0; j < ROW_SIZE; ++j) {
+            if (!playing) {
+                DrawRectangle(j*CELL_SIZE + CELL_SIZE / 2, i*CELL_SIZE + CELL_SIZE / 2, 1, 1, RED);
+            }
             if (AT(i, j)) {
                 DrawRectangle(j*CELL_SIZE, i*CELL_SIZE, CELL_SIZE, CELL_SIZE, BLUE);
             }
@@ -42,14 +50,14 @@ void draw_grid(void) {
 
 void random_grid(void) {
     clear_bg();
-    for (size_t i = 0; i < COL_SIZE/8; ++i) {
-        for (size_t j = 0; j < ROW_SIZE/8; ++j) {
+    for (size_t i = 0; i < COL_SIZE; ++i) {
+        for (size_t j = 0; j < ROW_SIZE; ++j) {
             AT(i, j) = (custom_bad_random() + 1) % 2;
         }
     }
 }
 
-void step(void) {
+void step(int *bs) {
     // B3/S1234
     static bool ngrid[GRID_SIZE] = {0};
     for (size_t i = 0; i < COL_SIZE; ++i) {
@@ -68,17 +76,65 @@ void step(void) {
                 }
             }
             if (AT(i, j)) {
-                ATS(ngrid, i, j) = count >= 1 && count <= 4 ? true : false;
+                ATS(ngrid, i, j) = IS_SET_S(bs, count);
             } else {
-                ATS(ngrid, i, j) = count == 3;
+                ATS(ngrid, i, j) = IS_SET_B(bs, count);
             }
         }
     }
     memcpy(grid, ngrid, GRID_SIZE);
 }
 
-int main(void)
+void bs_error(const char *bs_string) {
+    fprintf(stderr, "Faulty BS rulestring: %s\n", bs_string);
+    exit(1);
+}
+
+void consume(const char *bs_string, size_t *i, char expected) {
+    if (bs_string[*i] != expected) {
+        bs_error(bs_string);
+    }
+    *i += 1;
+}
+
+void append(char digit, int *bs, bool should_shift) {
+    if (should_shift) {
+        SET_S(bs, digit - '0');
+    } else {
+        SET_B(bs, digit - '0');
+    }
+}
+
+void parse_bs_rulestring(const char *bs_string, int *bs) {
+    size_t i = 0;
+    size_t L = strlen(bs_string);
+    consume(bs_string, &i, 'B');
+    while (i < L && bs_string[i] != '/') {
+        if (bs_string[i] < '0' || bs_string[i] > '9') {
+            bs_error(bs_string);
+        }
+        append(bs_string[i], bs, false);
+        ++i;
+    }
+    consume(bs_string, &i, '/');
+    consume(bs_string, &i, 'S');
+    while (i < L && bs_string[i] != '\0') {
+        if (bs_string[i] < '0' || bs_string[i] > '9') {
+            bs_error(bs_string);
+        }
+        append(bs_string[i], bs, true);
+        ++i;
+    }
+}
+
+int main(int argc, const char** argv)
 {
+    int bs = 0;
+    if (argc != 2) {
+        printf("Usage: %s [bs-rulestring]\n", argv[0]);
+        return 1;
+    }
+    parse_bs_rulestring(argv[1], &bs);
     srand(time(NULL));
     InitWindow(W, H, "Raylib Template");
     SetTargetFPS(60);
@@ -90,25 +146,39 @@ int main(void)
         }
     }
 
-    random_grid();
+    // random_grid();
     float prev_t = 0;
     float t = 0.f;
+    bool play = false;
+    bool down = false;
     while (!WindowShouldClose()) {
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            clear_bg();
+            down = true;
+        }
+        if (down && IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            down = false;
+        }
+        if (down) {
             Vector2 mouse = GetMousePosition();
+            ATW(mouse.y, mouse.x) = true;
         }
         if (IsKeyPressed(KEY_SPACE)) {
+            play = !play;
+        }
+        if (IsKeyPressed(KEY_R)) {
             random_grid();
         }
-        if (t - prev_t > 0.01f) {
+        if (IsKeyPressed(KEY_C)) {
+            clear_bg();
+        }
+        if (play && t - prev_t > 0.01f) {
             prev_t = t;
-            step();
+            step(&bs);
         }
 
         BeginDrawing();
-        draw_grid();
         ClearBackground(GetColor(0x181818FF));
+        draw_grid(play);
         EndDrawing();
         t += DT;
     }
